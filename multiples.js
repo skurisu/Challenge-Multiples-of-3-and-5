8412499edc7fa1f81caf2ec03c54f07c44b826f8
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-01-14 19:09:30 -1000

    multiples of 3 and 5 test passed

--- multiples.js
+++ multiples.js
@@ -6,6 +6,11 @@
 exports.sumOfAMultiple = function(n){
   var sum = 0;
   // do your work here
-  
+  for(var i = 1; i < n; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+      sum += i;
+    }
+  }
   return sum;
 };
+  
